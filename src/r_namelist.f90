c7fd864a6b2d2b16c11982ee443f762f106f0f6f
Jinbo Wang <jinbow@gmail.com>
2015-11-04 00:32:48 -0800

    delete useKh

--- src/r_namelist.f90
+++ src/r_namelist.f90
@@ -4,7 +4,7 @@ subroutine read_namelist()
     use global, only : casename,path2uvw,fn_UVEL,fn_VVEL,&
         fn_WVEL,dt,dt_reinit,tend,dt_case,fn_THETA,fn_SALT,&
         fn_GAMMA,dt_mld,pickupFreq,pickup,dumpFreq,diagFreq,tstart,fn_parti_init,&
-        target_density,useMLD,useKh,saveTSG,vel_stationary,Khdiff,Kvdiff,NPP,Npts,output_dir,&
+        target_density,useMLD,vel_stationary,Khdiff,Kvdiff,NPP,Npts,output_dir,&
         fn_PHIHYD
     implicit none
 !    integer*8, intent(in) :: inml
@@ -13,18 +13,17 @@ subroutine read_namelist()
     namelist /PARAM/ casename,path2uvw,fn_UVEL,fn_VVEL,&
         fn_WVEL,dt,tend,fn_THETA,fn_SALT,&
         fn_GAMMA,pickup,pickupFreq,dumpFreq,diagFreq,tstart,fn_parti_init,&
-        target_density,dt_reinit,dt_mld,dt_case,useMLD,saveTSG,vel_stationary,&
-        useKh,Khdiff,Kvdiff,NPP,Npts,output_dir,fn_PHIHYD
+        target_density,dt_reinit,dt_mld,dt_case,useMLD,vel_stationary,&
+        Khdiff,Kvdiff,NPP,Npts,output_dir,fn_PHIHYD
 
-    read (*,NML=PARAM)
-
-    !from the namelist file
+!    read (*,NML=PARAM)
+!from the namelist file
 !    write(fn,"(I4.4)") inml
 !    print*, 'NML.DP_'//fn
-!    OPEN (UNIT=1, FILE=trim('NML.DP_'//fn))
-!    OPEN (UNIT=212, FILE='data.nml')
-!    read (212,NML=PARAM) !from the namelist file
-!    close(212)
+    OPEN (UNIT=212, FILE='data.nml')
+    read (212,NML=PARAM) !from the namelist file
+    close(212)
+
 end subroutine read_namelist
 
 
