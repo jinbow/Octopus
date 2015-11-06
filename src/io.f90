383f5fd9e7f1ea8093979c1e16f2bbe580515756
Jinbo Wang <jinbow@gmail.com>
2015-11-05 23:39:51 -0800

    move save_data to io.f90

--- src/io.f90
+++ src/io.f90
@@ -202,5 +202,57 @@ subroutine load_grid()
 
 end subroutine load_grid
 
+subroutine save_data(IPP)
+#include "cpp_options.h"
+!output particle data
+    use omp_lib
+    use global, only: casename,tt,fn_ids,xyz,tsg,&
+        Npts,parti_mld,DumpClock,NPP,output_dir,grad
+    implicit none
+    character(len=128) :: fn
+    character(len=16) :: fn1
+    integer*8 :: iwrite
+    integer*8,intent(in) :: IPP
+
+    iwrite=int(tt/DumpClock)+1
+
+    write(fn,"(I10.10)") iwrite
+    write(fn1,"(I4.4)") IPP
+
+!$OMP PARALLEL SECTIONS
+
+    !$OMP SECTION
+    open(fn_ids(1,IPP),file=trim(output_dir)//'/'//trim(casename)//'_'//trim(fn1)//'.XYZ.'//trim(fn)//'.data',&
+        access='direct',form='unformatted', convert='BIG_ENDIAN',recl=3*4*Npts,status='unknown')
+    write(fn_ids(1,IPP),rec=1) real(xyz(:,:,IPP),4)
+    close(fn_ids(1,IPP))
+
+#ifdef saveTSG
+    !$OMP SECTION
+        open(fn_ids(2,IPP),file=trim(output_dir)//'/'//trim(casename)//'_'//trim(fn1)//'.TSG.'//trim(fn)//'.data',&
+            access='direct',form='unformatted',convert='BIG_ENDIAN',recl=4*4*Npts,status='unknown')
+        write(fn_ids(2,IPP),rec=1) real(tsg(:,:,IPP),4)
+        close(fn_ids(2,IPP))
+
+#endif
+
+#ifdef useMLD
+    !$OMP SECTION
+        open(fn_ids(3,IPP),file=trim(output_dir)//'/'//trim(casename)//'_'//trim(fn1)//'.MLD.'//trim(fn)//'.data',&
+            access='direct',form='unformatted',convert='BIG_ENDIAN',recl=4*Npts,status='unknown')
+        write(fn_ids(3,IPP),rec=1) real(parti_mld(:,IPP),4)
+        close(fn_ids(3,IPP))
+#endif
+
+#ifdef saveGradient
+    !$OMP SECTION
+    open(fn_ids(4,IPP),file=trim(output_dir)//'/'//trim(casename)//'_'//trim(fn1)//'.GRAD.'//trim(fn)//'.data',&
+        access='direct',form='unformatted', convert='BIG_ENDIAN',recl=5*4*Npts,status='unknown')
+    write(fn_ids(4,IPP),rec=1) real(grad(:,:,IPP),4)
+    close(fn_ids(4,IPP))
+#endif
+!$OMP END PARALLEL SECTIONS
+
+end subroutine save_data
 
 
