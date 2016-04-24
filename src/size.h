ca8e990f02c2ccf6fed8d49b28e2370f63c98cc4
Jinbo Wang <jinbow@gmail.com>
2016-04-24 00:23:01 -0700

    changed size.h

--- src/size.h
+++ src/size.h
@@ -1,3 +1,3 @@
 integer*8, parameter :: Nx=2160, Ny=320, Nz=42
-integer*8, parameter :: Nrecs=5
-real*8, parameter :: dt_file=60 ! 1day state
+integer*8, parameter :: Nrecs=2191
+real*8, parameter :: dt_file=86400 ! 1day state
