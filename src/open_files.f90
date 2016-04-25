fb3fe46935c4a7f7ff7c0ea106cef8b77398501d
Mars Creature <jinbow@gmail.com>
2016-04-25 14:44:38 -0700

    GAMMA issue, open_files.f90

--- src/open_files.f90
+++ src/open_files.f90
@@ -1,4 +1,5 @@
 subroutine open_files()
+#include "cpp_options.h"
     use global,only:fn_uvwtsg_ids,fn_ids,fn_id_mld,path2uvw,fn_UVEL,&
                     fn_VVEL,fn_WVEL,fn_THETA,fn_SALT,fn_GAMMA,fn_PHIHYD,&
                     Nx,Ny,Nz,fn_MLD
@@ -19,9 +20,13 @@ subroutine open_files()
     open(fn_uvwtsg_ids(5),file=trim(path2uvw)//trim(fn_SALT),&
         form='unformatted',access='direct',convert='BIG_ENDIAN',&
         status='old',recl=4*Nx*Ny)
+
+    if (trim(fn_GAMMA) .ne. '') then
     open(fn_uvwtsg_ids(6),file=trim(path2uvw)//trim(fn_GAMMA),&
         form='unformatted',access='direct',convert='BIG_ENDIAN',&
         status='old',recl=4*Nx*Ny)
+    endif
+
     if (trim(fn_PHIHYD) .ne. '') then
     open(fn_uvwtsg_ids(7),file=trim(path2uvw)//trim(fn_PHIHYD),&
         form='unformatted',access='direct',convert='BIG_ENDIAN',&
