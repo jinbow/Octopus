908f238b98861ac07cc60bc3cff832eb6662a8d9
Jinbo Wang <jinbow@gmail.com>
2017-09-21 17:47:03 -0700

    uncommented get_velocity.f90 to include horizontal velocity, changed data.nml, cleaned cpp_options.h

--- src/cpp_options.h
+++ src/cpp_options.h
@@ -1,10 +1,8 @@
 #define saveTSG
 #undef isArgo
 #define isGlider
-#define instrument glider
 #undef reflective_continent
 #undef use_Laplacian_diffusion
-#undef reflective_continent
 #undef use_mixedlayer_shuffle
 #undef saveGradient
 #undef reflective_meridional_boundary
